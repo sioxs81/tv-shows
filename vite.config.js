e706495e105234ddc54aad8df6e2fdf92f57b6b1
Sioxs81 <demiansand@gmail.com>
2026-05-07 14:11:23 -0600

    corregir config de vite y orden del @import en css

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 // vite.config.js
 export default {
     base: "/tv-shows/",   // ← debe coincidir con el nombre de tu repo
+    root: "src", 
     build: {
-        outDir: "dist",
+        outDir: "..dist",
         rollupOptions: {
             input: "src/index.html",
         },
